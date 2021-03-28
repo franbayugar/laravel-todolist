d3f6331de5811f4c0c0807054ab580ad19062446
franbayugar <franbayu_96@hotmail.com>
2021-03-28 12:49:08 -0300

    inital comit

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="todoListContainer">
+    <div class="heading">
+      <h2 id="title">Todo List</h2>
+      <add-item-form v-on:reloadList="getList()" />
+    </div>
+    <list-view :items="items" v-on:reloadList="getList()" />
+  </div>
+</template>
+
+<script>
+import AddItemForm from "./addItemForm.vue";
+import ListView from "./listView.vue";
+export default {
+  components: {
+    AddItemForm,
+    ListView,
+  },
+  data: function () {
+    return {
+      items: [],
+    };
+  },
+  methods: {
+    getList() {
+      axios
+        .get("api/items")
+        .then((response) => {
+          this.items = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.getList();
+  },
+};
+</script>
+
+<style scoped>
+.todoListContainer {
+  width: 350px;
+  margin: auto;
+}
+.heading {
+  background: #e6e6e6;
+  padding: 10px;
+}
+#title {
+  text-align: center;
+}
+* {
+  font-family: "Trebuchet MS", "Lucida Sans Unicode", "Lucida Grande",
+    "Lucida Sans", Arial, sans-serif;
+}
+</style>>
+
+</style>
---
+++ resources/js/vue/listView.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-for="(item, index) in items" :key="index">
+      <list-Item
+        :item="item"
+        class="item"
+        v-on:itemChanged="$emit('reloadList')"
+      />
+    </div>
+  </div>
+</template>
+ 
+<script>
+import ListItem from "./listItem";
+export default {
+  props: ["items"],
+  components: {
+    ListItem,
+  },
+};
+</script>
+<style scoped>
+.item {
+  background: #e6e6e6;
+  padding: 5px;
+  margin-top: 5px;
+}
+</style>
